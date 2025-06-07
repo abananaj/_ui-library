f122dda7e77d8e0eafb8b7bba60d66c68b87596a
abananaj <anna@theatrum.dev>
2025-06-07 13:50:06 -0700

    added snackbar

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
 import Tooltip from './theatrum-ui/tooltip';
 import Dropdown from './theatrum-ui/dropdown';
 import Tabs from './theatrum-ui/tabs';
-
-// create tooltip
-// const tooltip = new Tooltip(document.querySelector('.tooltip'));
-// tooltip.init();
+import Snackbar from './theatrum-ui/snackbar';
 
 // CREATE TOOLTIP
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
-
 tooltip.init();
 
 // CREATE DROPDOWNS
 const dropdowns = document.querySelectorAll('.dropdown');
-
 dropdowns.forEach(dropdown => {
     const instance = new Dropdown(dropdown);
     instance.init();
 })
 
-
 // CREATE TABS
 const tabs = new Tabs(document.querySelector('.tabs'));
 tabs.init();
+
+// CREATE SNACKBAR
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    snackbar.show('you clicked me');
+})
