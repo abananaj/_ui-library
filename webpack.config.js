726e575bd24d7e59edc4a3b4b419a633aea308e0
abananaj <anna@theatrum.dev>
2025-06-07 00:12:54 -0700

    add comments to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,29 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist/assets'),
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    publicPath: '/assets/'
+    static: path.resolve(__dirname, 'dist'),
+    devMiddleware: {
+      publicPath: '/assets/'
+    }
   },
-  module: {
+ module: {
     rules: [{
+      // any javascript files
       test: /\.js$/,
+      // except in node_modules, only process your code
       exclude: /node_modules/,
+      // specify loader
       use: {
         loader: 'babel-loader',
         options: {
+          // specify babel preset
           presets: ['@babel/preset-env']
         }
       }
