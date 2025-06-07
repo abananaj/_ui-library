926647a26710b61661fd9ae51f733022a29b6f13
abananaj <anna@theatrum.dev>
2025-06-07 13:23:00 -0700

    added dropdown

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Tooltip from './theatrum-ui/tooltip';
+import Dropdown from './theatrum-ui/dropdown';
 
 // create tooltip
 // const tooltip = new Tooltip(document.querySelector('.tooltip'));
@@ -8,3 +9,11 @@ import Tooltip from './theatrum-ui/tooltip';
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
 
 tooltip.init();
+
+// CREATE DROPDOWNS
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const instance = new Dropdown(dropdown);
+    instance.init();
+})
